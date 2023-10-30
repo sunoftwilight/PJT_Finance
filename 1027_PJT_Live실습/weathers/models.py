16928c8da7964d50cdd661674d2b5fdeb1da2178
sun <v00417@naver.com>
2023-10-30 11:33:47 +0900

    1030

--- 1027_PJT_Live실습/weathers/models.py
+++ 1027_PJT_Live실습/weathers/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Weather(models.Model):
+    temp = models.FloatField()          # 데이터 측정 시간
+    feels_like = models.FloatField()    # 체감 온도(기본값: 켈빈)
+    dt_txt = models.DateTimeField()     # 데이터 측정 시간
